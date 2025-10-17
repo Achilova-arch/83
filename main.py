0d9c4b380a6076c3417a2caca3ed44cd85446db9
Achilova-arch <asalachilova96@gmail.com>
2025-10-17 19:04:46 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+son=1
+
+while son <=101:
+    son +=1
+    if son==50:
+        break
+    else:
+        print(son)
+
+son=0
+
+while son <=20:
+    son +=1
+    if son % 2 == 0:
+       print(son)
+    else:
+       continue
+
+sonlar=input("Son kriting:")
+
+while sonlar in range(5):
+    if sonlar % 2 == 0:
+       print(sonlar)
+    else:
+       continue  
+
+
+    
